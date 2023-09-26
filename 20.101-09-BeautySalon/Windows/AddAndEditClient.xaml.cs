aeb862a2d634526fed5790b22bb54a69bfe958c1
Alex <alexiv.04@mail.ru>
2023-09-26 20:27:26 +0700

    finish add and edit client window

--- 20.101-09-BeautySalon/Windows/AddAndEditClient.xaml.cs
+++ 20.101-09-BeautySalon/Windows/AddAndEditClient.xaml.cs
@@ -1,4 +1,5 @@
-﻿using _20._101_09_BeautySalon.Models;
+﻿using _20._101_09_BeautySalon.Classes;
+using _20._101_09_BeautySalon.Models;
 using _20._101_09_BeautySalon.Pages;
 using Microsoft.Win32;
 using Mono.Cecil;
@@ -54,7 +55,17 @@ namespace _20._101_09_BeautySalon.Windows
 
         private void AddAndEditClientWindow_Closed(object sender, EventArgs e)
         {
-
+            try
+            {
+                if (clientPage != null)
+                {
+                    clientPage.Load(); // Вызываем метод на ClientPage
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void btnEnterImage_Click(object sender, RoutedEventArgs e)
@@ -87,57 +98,201 @@ namespace _20._101_09_BeautySalon.Windows
 
         private void btnDelImage_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                string defaultImagePath = (string)FindResource("defaultImage"); // Получаем путь к изображению из ресурсов
+                if (!string.IsNullOrEmpty(defaultImagePath))
+                {
+                    BitmapImage defaultImage = new BitmapImage(new Uri(defaultImagePath)); // Создаем BitmapImage из пути
+                    imgPhoto.Source = defaultImage; // Устанавливаем изображение по умолчанию
+                }
+                client.PhotoPath = null;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void tbFirstName_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-
+            e.Handled = !ValidatorExtensions.IsValidFIO(e.Text);
         }
 
         private void tbLastName_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-
+            e.Handled = !ValidatorExtensions.IsValidFIO(e.Text);
         }
 
         private void tbPatronymic_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-
+            e.Handled = !ValidatorExtensions.IsValidFIO(e.Text);
         }
 
         private void tbEmail_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-
+            e.Handled = !ValidatorExtensions.IsValidEmailAddress(e.Text);
         }
 
         private void Phone_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-
+            e.Handled = !ValidatorExtensions.IsValidPhone(e.Text);
         }
 
+        private void RefrData()
+        {
+            client.Birthday = (DateTime)dpBirthDate.SelectedDate;
+            client.GenderCode = (cmbGender.SelectedIndex + 1).ToString();
+        }
+
+        private void SaveInDB(string text)
+        {
+            db.SaveChanges();
+            MessageBox.Show(text, "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.Close();
+        }
+
+        private void Errors(bool expression, StringBuilder errors, string message)
+        {
+            if (expression)
+            {
+                errors.AppendLine(message);
+            }
+        }
         private void btnAddClient_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                StringBuilder errors = new StringBuilder();
+                Errors(cmbGender.SelectedItem == null, errors, "Выберите пол клиента!");
+                Errors(tbFirstName.Text.Count() > 50, errors, "Фамилия не может быть длиннее 50 символов!");
+                Errors(tbLastName.Text.Count() > 50, errors, "Имя не может быть длиннее 50 символов!");
+                Errors(tbPatronymic.Text.Count() > 50, errors, "Отчество не может быть длиннее 50 символов!");
+                Errors(ValidatorExtensions.IsValidEmailAddress(tbEmail.Text), errors, "Email адрес не действителен");
+                Errors(tbFirstName.Text == ""
+                    || tbLastName.Text == ""
+                    || tbPhone.Text == "", errors, "Не заполнена важная ифнормация!");
+                if (errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString());
+                    return;
+                }
+                if (currentClient == null)
+                {
+                    client.RegistrationDate = DateTime.Now;
+                    RefrData();
+                    db.Client.Add(client);
+                    SaveInDB("Добавление информации о клиенте завершено");
+                }
+                else
+                {
+                    RefrData();
+                    SaveInDB("Обновление информации о клиенте завершено");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void btnDeleteClient_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                if (client.ClientService.Count > 0)
+                {
+                    MessageBox.Show("Удаление не возможно, т.к. есть информация о посещении", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                    return;
+                }
+                foreach (Tag tag in client.Tag)
+                {
+                    db.Tag.Remove(tag);
+                }
+                db.Client.Remove(client);
+                db.SaveChanges();
+                MessageBox.Show("Удаление информации об клиенте завешено!", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void tbColorTag_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-
+            //e.Handled = !ValidatorExtensions.IsValidColor(e.Text);
+            if (tbColorTag.Text.Length >= 6 && !e.Text.Equals("\b"))
+            {
+                e.Handled = true;
+            }
+            else
+            {
+                e.Handled = !ValidatorExtensions.IsValidColor(e.Text);
+            }
         }
 
         private void btnAddTag_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                StringBuilder errors = new StringBuilder();
+                Errors(tbColorTag.Text == "", errors, "Введите цвет!");
+                Errors(tbTitleTag.Text == "", errors, "Заголовог тега!");
+                if (errors.Length > 0)
+                {
+                    MessageBox.Show(errors.ToString());
+                    return;
+                }
+                Tag tag = new Tag
+                {
+                    ID = db.Tag.Any() ? db.Tag.Max(t => t.ID) + 1 : 1,
+                    Title = tbTitleTag.Text,
+                    Color = tbColorTag.Text
+                };
+                client.Tag.Add(tag);
+                db.SaveChanges();
+                tbTitleTag.Text = ""; tbColorTag.Text = "";
+                LViewTags.ItemsSource = client.TagList;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void btnDelTag_Click(object sender, RoutedEventArgs e)
         {
-
+            int count = LViewTags.SelectedItems.Count;
+            if (count > 0)
+            {
+                if (MessageBox.Show($"Вы действительно хотите удалить {count} запись(ей)?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    try
+                    {
+                        var selected = LViewTags.SelectedItems.Cast<Tag>().ToArray();
+                        foreach (var item in selected)
+                        {
+                            db.Tag.Remove(item);
+                        }
+                        db.SaveChanges();
+                        string about;
+                        if (count == 1) about = "Тег удален!";
+                        else about = "Теги удалены!";
+                        MessageBox.Show(about, "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                        LViewTags.ItemsSource = client.TagList;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString(), "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите теги для удаления", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
     }
 }
